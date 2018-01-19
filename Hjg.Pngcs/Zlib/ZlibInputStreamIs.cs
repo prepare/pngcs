7c3a488aa79245c6c4aeaebc1af019a44a5838e3
win <wintercoredev@gmail.com>
2018-01-19 07:20:46 +0700

    remove unused namespace, add notes

--- Hjg.Pngcs/Zlib/ZlibInputStreamIs.cs
+++ Hjg.Pngcs/Zlib/ZlibInputStreamIs.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.IO.Compression;
+﻿//Apache2, 2012, Hernan J Gonzalez, https://github.com/leonbloy/pngcs
 
 #if SHARPZIPLIB
 
